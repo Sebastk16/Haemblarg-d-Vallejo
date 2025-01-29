18a298220496b3282e8aa208db16c22a68c58bbc
Sebastk16 <katsuroytofficial@gmail.com>
2025-01-29 12:29:35 -0500

    Descripción de los cambios

--- script.js
+++ script.js
@@ -31,12 +31,6 @@ sr.reveal('.features-container .box:nth-child(2)', { delay: 300, origin: 'top' }
 sr.reveal('.features-container .box:nth-child(3)', { delay: 500, origin: 'top' });
 sr.reveal('.features-container .box:nth-child(4)', { delay: 700, origin: 'top' });
 sr.reveal('.about-container .right', { origin: 'left' });
-sr.reveal('.courses-container', { origin: 'top' });
-sr.reveal('.team-container .card:nth-child(1)', { delay: 100, origin: 'top' });
-sr.reveal('.team-container .card:nth-child(2)', { delay: 300, origin: 'top' });
-sr.reveal('.team-container .card:nth-child(3)', { delay: 500, origin: 'top' });
-sr.reveal('.team-container .card:nth-child(4)', { delay: 700, origin: 'top' });
-sr.reveal('.team-container .card:nth-child(5)', { delay: 900, origin: 'top' });
 sr.reveal('.blog-container', { origin: 'left' });
 sr.reveal('.testimonial-container .box:nth-child(1)', { delay: 100, origin: 'top' });
 sr.reveal('.testimonial-container .box:nth-child(2)', { delay: 300, origin: 'top' });
@@ -44,3 +38,5 @@ sr.reveal('.testimonial-container .box:nth-child(3)', { delay: 500, origin: 'top
 sr.reveal('.contact-container .form div:first-child', { origin: 'right' });
 sr.reveal('.contact-container .form div:last-child', { origin: 'left' });
 sr.reveal('.footer-container', { origin: 'top' });
+
+
